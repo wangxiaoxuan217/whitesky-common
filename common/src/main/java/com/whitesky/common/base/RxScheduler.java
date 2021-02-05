bf0924242b6bf716c6768739c963e1ffdec22149
wangxin <xin.wang@whitesky.com.cn>
2021-02-05 11:58:40 +0800

    [feat][封装库]修复库结构2

---
+++ common/src/main/java/com/whitesky/common/base/RxScheduler.java
@@ -0,0 +1,20 @@
+package com.whitesky.common.base;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.ObservableTransformer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class RxScheduler
+{
+    /**
+     * 统一线程处理
+     *
+     * @param <T> 指定的泛型类型
+     * @return ObservableTransformer
+     */
+    public static <T> ObservableTransformer<T, T> Obs_io_main()
+    {
+        return upstream -> upstream.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+    }
+    
+}
