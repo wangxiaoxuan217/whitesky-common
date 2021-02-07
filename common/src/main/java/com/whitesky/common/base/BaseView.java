ed23a366c59573f992f37de7a92bf1c67f13ffcb
wangxin <xin.wang@whitesky.com.cn>
2021-02-07 14:45:04 +0800

    [feat][音圆]修复版本7

--- common/src/main/java/com/whitesky/common/base/BaseView.java
+++ common/src/main/java/com/whitesky/common/base/BaseView.java
@@ -5,13 +5,6 @@ import autodispose2.AutoDisposeConverter;
 public interface BaseView
 {
     /**
-     * 数据获取失败
-     * 
-     * @param errMessage
-     */
-    void onError(String errMessage);
-    
-    /**
      * 绑定Android生命周期 防止RxJava内存泄漏
      *
      * @param <T>
